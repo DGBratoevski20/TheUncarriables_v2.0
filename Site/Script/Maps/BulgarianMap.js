ecfd4773a0b97f78a893b90d122e79227eac9981
DGBratoevski20 <DGBratoevski20@codingburgas.bg>
2022-02-20 20:12:21 +0200

    Edit Bulgarian map image and construct it in folders

--- Site/Script/Maps/BulgarianMap.js
+++ Site/Script/Maps/BulgarianMap.js
@@ -1,24 +1,24 @@
 const a = document.getElementById("span1");
 a.addEventListener("click", function () {
-    window.open("./Index.html")
+    window.open("../Index.html")
 });
 const b = document.getElementById("span2");
 b.addEventListener("click", function () {
-    window.open("./Index.html")
+    window.open("../Index.html")
 });
 const c = document.getElementById("span3");
 c.addEventListener("click", function () {
-    window.open("./Index.html")
+    window.open("../Index.html")
 });
 const d = document.getElementById("span4");
 d.addEventListener("click", function () {
-    window.open("./Index.html")
+    window.open("../Index.html")
 });
 const f = document.getElementById("span5");
 f.addEventListener("click", function () {
-    window.open("./Index.html")
+    window.open("../Index.html")
 });
 const e = document.getElementById("span6");
 e.addEventListener("click", function () {
-    window.open("./Index.html")
+    window.open("../Index.html")
 });
