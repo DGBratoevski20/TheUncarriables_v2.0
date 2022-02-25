64ea7e07b6e776015bebc2c12cd15016763b2074
DGBratoevski20 <DGBratoevski20@codingburgas.bg>
2022-02-25 10:53:25 +0200

    Change Bulgarian map and add cities markers

--- Site/Script/Maps/BulgarianMap.js
+++ Site/Script/Maps/BulgarianMap.js
@@ -1,24 +1,37 @@
-const a = document.getElementById("span1");
-a.addEventListener("click", function () {
-    window.open("../Index.html")
-});
-const b = document.getElementById("span2");
-b.addEventListener("click", function () {
-    window.open("../Index.html")
-});
-const c = document.getElementById("span3");
-c.addEventListener("click", function () {
-    window.open("../Index.html")
-});
-const d = document.getElementById("span4");
-d.addEventListener("click", function () {
-    window.open("../Index.html")
-});
-const f = document.getElementById("span5");
-f.addEventListener("click", function () {
-    window.open("../Index.html")
-});
-const e = document.getElementById("span6");
-e.addEventListener("click", function () {
-    window.open("../Index.html")
-});
+var map = L.map('map').setView([42.75, 25.09], 7.5);
+	
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="http://osm.org/copyright%22%3EOpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([42.145, 24.75]).addTo(map)
+.bindPopup('<b>Plovdiv.</b><hr><i> The oldest city in Europe.</i>')
+.openPopup();
+
+L.marker([42.698, 23.32222222]).addTo(map)
+.bindPopup('<b>Sofia.</b><hr><i> The capital of Bulgaria.</i>')
+.openPopup();
+
+L.marker([43.20645, 27.91536333]).addTo(map)
+.bindPopup('<b>Varna.</b><hr><i> The sea capital of Bulgaria.</i>')
+.openPopup();
+
+L.marker([42.4923, 27.47136333]).addTo(map)
+.bindPopup('<b>Burgas.</b><hr><i> One of the biggest cities in Bulgaria.</i>')
+.openPopup();
+
+L.marker([43.0822, 25.63136333]).addTo(map)
+.bindPopup('<b>Veliko Tarnovo.</b><hr><i> The oldest capital in Bulgaria.</i>')
+.openPopup();
+
+L.marker([42.423871, 25.6272]).addTo(map)
+.bindPopup('<b>Stara Zagora.</b><hr><i> One of the biggest cities in Bulgaria.</i>')
+.openPopup();
+
+L.marker([43.8476, 25.954]).addTo(map)
+.bindPopup('<b>Ruse.</b><hr><i> One of the biggest cities in Bulgaria.</i>')
+.openPopup();
+
+L.marker([43.409, 24.617999]).addTo(map)
+.bindPopup('<b>Pleven.</b><hr><i>  One of the biggest cities in Bulgaria.</i>')
+.openPopup();
