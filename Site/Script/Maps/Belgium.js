318d02517811730344055d790c4c9ae3bb3a297f
DGBratoevski20 <DGBratoevski20@codingburgas.bg>
2022-02-26 19:11:17 +0200

    Add map for each country and fix the links in the geography cards

---
+++ Site/Script/Maps/Belgium.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([50.620948, 4.528359], 8);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="http://osm.org/copyright%22%3EOpenStreetMap</a> contributors'
+}).addTo(map);
---
+++ Site/Script/Maps/Netherlands.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([, ], );
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="http://osm.org/copyright%22%3EOpenStreetMap</a> contributors'
+}).addTo(map);
