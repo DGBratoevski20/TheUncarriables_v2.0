47b826cd1cebc1d4c56b59947a9db90caeb042d6
DGBratoevski20 <DGBratoevski20@codingburgas.bg>
2022-02-26 17:12:32 +0200

    Fix the bug with the Austiran map

--- Site/Script/Maps/Austrian.js
+++ Site/Script/Maps/Austrian.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([47.58186, 13.26485], 7);
+var map = L.map('map').setView([47.58186, 13.26485], 7.5);
 	
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="http://osm.org/copyright%22%3EOpenStreetMap</a> contributors'
