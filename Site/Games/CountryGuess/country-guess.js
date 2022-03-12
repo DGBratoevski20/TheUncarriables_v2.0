7b5d9988c986f7ceace519394acfb67f9130feff
HHChandar20 <HHChandar20@codingburgas.bg>
2022-03-12 08:28:37 +0200

    Add country guess game

---
+++ Site/Games/CountryGuess/country-guess.js
@@ -0,0 +1,817 @@
+var countryName = "";
+var game = true;
+
+function isEmpty(str) {
+    return (!str || str.length === 0 );
+}
+
+function Germany()
+{
+  countryName = "Germany";
+  capitalCity = "Berlin";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good Job!");
+      
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+
+
+  game = true;
+}
+
+function France()
+{
+  countryName = "France";
+  capitalCity = "Paris";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good Job!");
+      
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Afghanistan()
+{
+  countryName = "Afghanistan";
+  capitalCity = "Kabul";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good Job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Gabon()
+{
+  countryName = "Gabon";
+  capitalCity = "Libreville";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good Job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Portugal()
+{
+  countryName = "Portugal";
+  capitalCity = "Lisbon";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good Job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Bulgaria()
+{
+  countryName = "Bulgaria";
+  capitalCity = "Sofia";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good Job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Turkey()
+{
+  countryName = "Turkey";
+  capitalCity = "Ankara";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good Job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Malta()
+{
+  countryName = "Malta";
+  capitalCity = "Valletta";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good Job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Italy()
+{
+  countryName = "Italy";
+  capitalCity = "Rome";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good Job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Oman()
+{
+  countryName = "Oman";
+  capitalCity = "Muscat";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Honduras()
+{
+  countryName = "Honduras";
+  capitalCity = "Tegucigalpa";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function costaRica()
+{
+  countryName = "Costa Rica";
+  capitalCity = "San Jose";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Spain()
+{
+  countryName = "Spain";
+  capitalCity = "Madrid";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Qatar()
+{
+  countryName = "Qatar";
+  capitalCity = "Doha";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Iran()
+{
+  countryName = "Iran";
+  capitalCity = "Tehran";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function northKorea()
+{
+  countryName = "North Korea";
+  capitalCity = "Pyongyang";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function China()
+{
+  countryName = "China";
+  capitalCity = "Beijing";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
+
+function Angola()
+{
+  countryName = "Angola";
+  capitalCity = "Luanda";
+  while(game)
+  {
+    var country = prompt("Which country is this?", "...");
+    
+    if(country == countryName)
+    {
+      alert("Good job!");
+
+      while(game)
+      {
+        var capital = prompt("And the capital city is?", "...");
+    
+        if(capital == capitalCity)
+        {
+          alert("Well done!");
+          game = false;
+        }
+        else if(isEmpty(capital))
+        {
+          game = false;
+        }
+        else
+        {
+          alert("Don't give up buddy, come on!")
+        }
+      }
+      
+      game = false;
+    }
+    else if(isEmpty(country))
+    {
+      game = false;
+    }
+    else
+    {
+      alert("Don't give up buddy, come on!")
+    }
+  }
+  game = true;
+}
