b7f313e4f848f715026e24c9e1f79af947b3f12e
HHChandar20 <HHChandar20@codingburgas.bg>
2022-03-16 17:54:02 +0200

    Add Latvia tourist destinations

--- Site/Script/Maps/Latvia.js
+++ Site/Script/Maps/Latvia.js
@@ -16,6 +16,37 @@ let TouristDestinationsMarkerIcon = L.icon ({
     iconAnchor: [17.5,35],
 });
 
+//monuments
+L.marker([56.95864795575303, 23.71840773037556],{icon:TouristDestinationsMarkerIcon}).addTo(map)
+.bindPopup("<b>Jūrmala beach</b><hr><i>One of the most popular tourist destinations in Latvia.</i>")
+.openPopup();
+
+L.marker([56.569094395018396, 21.0285276586329],{icon:TouristDestinationsMarkerIcon}).addTo(map)
+.bindPopup("<b>Liepaja beach</b><hr><i>One of the most popular tourist destinations in Latvia.</i>")
+.openPopup();
+
+L.marker([56.969553279928455, 21.96325678436013],{icon:TouristDestinationsMarkerIcon}).addTo(map)
+.bindPopup('<b>Kuldiga</b><hr><i>One of the most popular tourist destinations in Latvia.</i>')
+.openPopup();
+
+L.marker([57.75897880672118, 22.604700654587802],{icon:TouristDestinationsMarkerIcon}).addTo(map)
+.bindPopup('<b>Cape Kolka</b><hr><i>One of the most popular tourist destinations in Latvia.</i>')
+.openPopup();
+
+L.marker([57.315182506260115, 25.27143806450294],{icon:TouristDestinationsMarkerIcon}).addTo(map)
+.bindPopup('<b>Cēsis Castle</b><hr><i>One of the most popular tourist destinations in Latvia.</i>')
+.openPopup();
+
+L.marker([56.41362310502395, 24.024801934954773],{icon:TouristDestinationsMarkerIcon}).addTo(map)
+.bindPopup('<b>Rundāle Palace Museum</b><hr><i>One of the most popular tourist destinations in Latvia.</i>')
+.openPopup();
+
+L.marker([57.183430249049785, 24.85963809994719],{icon:TouristDestinationsMarkerIcon}).addTo(map)
+.bindPopup('<b>Turaida castle</b><hr><i>One of the most popular tourist destinations in Latvia.</i>')
+.openPopup();
+
+
+// cities
 L.marker([57.39884497483879, 21.55014710659771],{icon:CitiesMarkerIcon}).addTo(map)
 .bindPopup('<b>Ventspils.</b><hr><i> One of the biggest cities in Latvia.</i>')
 .openPopup();
