e9114d8ba6a7a99cfd8f1f661020d73a0f982191
ipstoyanov20 <ipstoyanov20@codingburgas.bg>
2022-03-20 11:41:47 +0200

    Fix Randomazie Quiz

--- Site/Games/Quiz/quiz.js
+++ Site/Games/Quiz/quiz.js
@@ -1,11 +1,17 @@
 let right = document.querySelector('.rightPart');
 let left = document.querySelector('.leftPart');
 let Quest = document.querySelector('quiz-question');
+let p = document.querySelector('p');
 setInterval(function(){
     right.style.left  = window.pageYOffset + 'px';
     left.style.left  = -window.pageYOffset + 'px';
     
 }, 1)
+
+window.addEventListener('scroll', ()=>
+{
+    p.remove();
+})
 
 
 
@@ -14,35 +20,60 @@ setInterval(function(){
 //Question bank
 var questionBank= [
     {
-        question : 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
-        option : ['Asia','Africa','Europe','Australia'],
-        answer : 'Africa'
+        question : 'What is the name of the tallest mountain in the world?', // ready
+        option : ['Asia','Mount Everest','K5','Stara Mountain'],
+        answer : 'Mount Everest'
     },
     {
-        question : 'Garampani sanctuary is located at',
-        option : ['Junagarh, Gujarat','Diphu, Assam','Kohima, Nagaland','Gangtok, Sikkim'],
-        answer : 'Diphu, Assam'
+        question : 'What American city is the Golden Gate Bridge located in?', // ready
+        option : ['San Francisco','California','Alaska','Arizona'],
+        answer : 'San Francisco'
     },
     {
-        question : 'For which of the following disciplines is Nobel Prize awarded?',
-        option : ['Physics and Chemistry','Physiology or Medicine','Literature, Peace and Economics','All of the above'],
-        answer : 'All of the above'
+        question : 'Which country is also called The Netherlands? ', //ready
+        option : ['China','German','Antarctica','Holland'],
+        answer : 'Holland'
     },
     {
-        question : 'Hitler party which came into power in 1933 is known as',
-        option : ['Labour Party','Nazi Party','Ku-Klux-Klan','Democratic Party'],
-        answer : 'Nazi Party'
+        question : 'What is the name of the longest river in Africa? ',
+        option : ['The Amazon River','The Mississippi River','The Nile River','The Yenisei River'], // ready
+        answer : 'The Nile River'
     },
     {
-        question : 'First human heart transplant operation conducted by Dr. Christiaan Barnard on Louis Washkansky, was conducted in',
-        option : ['1967','1968','1958','1922'],
-        answer : '1967'
+        question : 'What country has the most natural lakes', // ready
+        option : ['China','Canada','Russia','USA'],
+        answer : 'Canada'
+    },
+    {
+        question : 'What city is known as the Glass Capital of the World? ', // ready
+        option : ['Chicago','Houston','Toledo','Los Angeles'],
+        answer : 'Toledo'
+    },
+    {
+        question : 'What ocean borders the state of Florida?', // ready
+        option : ['Pacific Ocean','Atlantic Ocean','Indian Ocean','Arctic Ocean'],
+        answer : 'Atlantic Ocean'
+    },
+    {
+        question : 'What is the name of the largest lake in the US?', // ready
+        option : ['Lake Albert','Lake Superior','Lake Edward','Lake Kivu'],
+        answer : 'Lake Superior'
+    },
+    {
+        question : 'What river flows through the Grand Canyon?', // ready
+        option : ['Colorado River','Lena River','Purus River','Yukon River'],
+        answer : 'Colorado River'
+    },
+    {
+        question : 'How many total islands does Hawaii have?', // ready
+        option : ['132','68','47','231'],
+        answer : '132'
     }
 ]
 
-var question= document.getElementById('question');
+var questionIN= document.getElementById('question');
 var quizContainer= document.getElementById('quiz-container');
-var scorecard= document.getElementById('scorecard');
+var scoreboard= document.getElementById('scoreboard');
 var option0= document.getElementById('option0');
 var option1= document.getElementById('option1');
 var option2= document.getElementById('option2');
@@ -51,24 +82,66 @@ var next= document.querySelector('.next');
 var points= document.getElementById('score');
 var span= document.querySelectorAll('span');
 var i=0;
+var randAr = [];
 var score= 0;
-
+var rand;
+var counterqustion = 0;
+let bool = true;
+function load()
+{
+    Rand();
+}
+function Rand()
+{
+    rand = Math.floor(Math.random()*10);
+    for(let j of randAr)
+    {
+        console.log(j)
+        if(j == rand)
+        {
+            bool = false;
+            break;
+        }
+        else{
+            bool = true;
+        }
+        
+    }
+    
+    
+    if(bool)
+    {
+        randAr[i] = rand;
+        i++;
+        console.log(randAr)
+        bool= true;
+        displayQuestion();
+        
+    }
+    else{
+        Rand();
+    }
+   
+    
+}
 //function to display questions
 function displayQuestion(){
     for(var a=0;a<span.length;a++){
         span[a].style.background='none';
     }
-    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
-    option0.innerHTML= questionBank[i].option[0];
-    option1.innerHTML= questionBank[i].option[1];
-    option2.innerHTML= questionBank[i].option[2];
-    option3.innerHTML= questionBank[i].option[3];
-    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+    questionIN.innerHTML = questionBank[rand].question;
+    option0.innerHTML= questionBank[rand].option[0];
+    option1.innerHTML= questionBank[rand].option[1];
+    option2.innerHTML= questionBank[rand].option[2];
+    option3.innerHTML= questionBank[rand].option[3];
+    stat.innerHTML= "Question"+' '+(counterqustion+1)+' '+'of'+' '+questionBank.length;
+    counterqustion++;
+    
 }
 
 //function to calculate scores
 function calcScore(e){
-    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
+    if(e.innerHTML===questionBank[rand].answer && score<questionBank.length)
     {
         score= score+1;
         document.getElementById(e.id).style.background= 'limegreen';
@@ -81,10 +154,10 @@ function calcScore(e){
 
 //function to display next question
 function nextQuestion(){
-    if(i<questionBank.length-1)
+    if(counterqustion < questionBank.length)
     {
-        i=i+1;
-        displayQuestion();
+        
+        Rand();
     }
     else{
         points.innerHTML= score+ '/'+ questionBank.length;
@@ -116,4 +189,4 @@ function checkAnswer(){
 }
 
 
-displayQuestion();
+//displayQuestion();
